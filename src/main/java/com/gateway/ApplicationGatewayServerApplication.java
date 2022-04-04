6965ffa8300c31ecd9a082c7029c3863143b54e8
ranjit2020 <jadhavranjit2017@gmail.com>
2022-04-04 19:38:57 +0530

    service for gateway server

---
+++ src/main/java/com/gateway/ApplicationGatewayServerApplication.java
@@ -0,0 +1,13 @@
+package com.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationGatewayServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationGatewayServerApplication.class, args);
+	}
+
+}
